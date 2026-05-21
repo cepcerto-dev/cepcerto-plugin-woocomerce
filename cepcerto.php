9a2bf0c618b40903d29dee1407b391f0cc73bbaa
BD-Desktop <dev@ceperto.com.br>
2026-05-20 22:46:19 -0300

    refactor: :recycle: wordpress version updated

--- cepcerto.php
+++ cepcerto.php
@@ -13,7 +13,7 @@
  * Requires Plugins: woocommerce
  * Requires PHP: 7.4
  * Requires at least: 6.0
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * WC requires at least: 7.0
  * WC tested up to: 9.4
  * WC requires HPOS: yes
